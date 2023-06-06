ba4beb6c1aeb5d0f3a968287ff3855127d43c38a
Joren <97386268+Aebal34@users.noreply.github.com>
2023-06-06 13:05:26 +0200

    Tienda queries 41/41 done

--- MySQL.queries.exercici_tienda.sql
+++ MySQL.queries.exercici_tienda.sql
@@ -49,7 +49,34 @@ SELECT p.nombre AS nombre_producto, p.precio, f.nombre AS nombre_fabricante FROM
 -- Tienda queries - 25
 SELECT p.nombre AS nombre_producto, p.precio, f.nombre AS nombre_fabricante FROM producto AS p JOIN fabricante AS f ON p.codigo_fabricante = f.codigo WHERE p.precio = (SELECT MAX(precio) FROM producto);
 -- Tienda queries - 26
-
-
-
-
+SELECT producto.* FROM producto LEFT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Lenovo';
+-- Tienda queries - 27
+SELECT producto.* FROM producto LEFT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Crucial' AND producto.precio > 200;
+-- Tienda queries - 28
+SELECT producto.* FROM producto LEFT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE (fabricante.nombre = 'Asus' OR fabricante.nombre = 'Seagate' OR fabricante.nombre = 'Hewlett-Packard');
+-- Tienda queries - 29
+SELECT producto.* FROM producto LEFT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre IN ('Asus', 'Seagate', 'Hewlett-Packard');
+-- Tienda queries - 30
+SELECT producto.nombre, producto.precio FROM PRODUCTO LEFT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre LIKE '%e';
+-- Tienda queries - 31
+SELECT p.nombre, p.precio FROM producto AS p LEFT JOIN fabricante AS f ON f.codigo = p.codigo_fabricante WHERE f.nombre LIKE '%w%';
+-- Tienda queries - 32
+SELECT p.nombre, p.precio, f.nombre AS fabricante_nombre FROM producto AS p LEFT JOIN fabricante AS f ON f.codigo = p.codigo_fabricante WHERE p.precio > 180 ORDER BY p.precio DESC, p.nombre ASC;
+-- Tienda queries - 33
+SELECT DISTINCT fabricante.* FROM fabricante RIGHT JOIN producto ON producto.codigo_fabricante = fabricante.codigo;
+-- Tienda queries - 34
+SELECT * FROM fabricante LEFT JOIN producto ON producto.codigo_fabricante = fabricante.codigo;
+-- Tienda queries - 35
+SELECT fabricante.* FROM fabricante LEFT JOIN producto ON producto.codigo_fabricante = fabricante.codigo WHERE producto.codigo_fabricante IS NULL;
+-- Tienda queries - 36
+SELECT producto.* FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo AND fabricante.nombre = 'Lenovo';
+-- Tienda queries - 37
+SELECT producto.* FROM producto, fabricante WHERE fabricante.codigo = producto.codigo_fabricante AND producto.precio =  (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo FROM fabricante WHERE nombre = 'Lenovo'));
+-- Tienda queries - 38
+SELECT nombre FROM producto  WHERE precio = (SELECT MAX(precio) FROM producto  JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Lenovo');
+-- Tienda queries - 39
+SELECT nombre FROM producto WHERE precio = (SELECT MIN(precio) FROM producto JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Hewlett-Packard');
+-- Tienda queries - 40
+SELECT * FROM producto WHERE precio >= (SELECT MAX(precio) FROM producto  JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Lenovo');
+-- Tienda queries - 41
+SELECT * FROM producto LEFT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Asus' AND precio > (SELECT AVG(precio) FROM producto RIGHT JOIN fabricante ON fabricante.codigo = producto.codigo_fabricante WHERE fabricante.nombre = 'Asus');
